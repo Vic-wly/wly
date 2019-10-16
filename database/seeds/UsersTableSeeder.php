4ba8a7cbd2be3fa965da0231c5a078833d45440d
wanglaiyong <549608057@qq.com>
2019-10-16 06:34:03 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'wly';
         $user->email = 'wanglaiyong@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
